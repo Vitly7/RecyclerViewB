b91ce02565feffbfc6ba549c0224fc1cf18bdc98
Vitly7 <111552374+Vitly7@users.noreply.github.com>
2024-06-19 20:11:13 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/recyclerviewb/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewb/MainActivity.java
@@ -22,6 +22,7 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
